5c92b2807982c129bebeef5dbdc7bd2d450929ea
joaoleandro <new.joaoleandro@gmail.com>
2023-10-04 20:31:03 -0300

    firist commit

---
+++ main.js
@@ -0,0 +1,37 @@
+const dadosJson = './dados.json'
+const clube = []
+const titulos = []
+fetch(dadosJson)
+.then((response) => response.json())
+.then((json) => {
+
+    const x = json.titulos
+    x.map((todo) => {
+        clube.push(todo.nome)
+        titulos.push(todo.numero_titulos)
+    })
+
+    const ctx = document.getElementById('grafico')
+
+new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: clube,
+    datasets: [{
+      label: 'Times com mais titulos da Champios League',
+      data: titulos,
+      borderWidth: 1,
+      backgroundColor: "#08088A"
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+})
+})
+
+
